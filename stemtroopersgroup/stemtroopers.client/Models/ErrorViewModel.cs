3e087fdb277b38d7f2719d31a88b5e401e55aebc
frede <frederickesplanda@outlook.com>
2021-07-04 20:34:50 +1000

    initialize

---
+++ stemtroopersgroup/stemtroopers.client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace stemtroopers.client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
